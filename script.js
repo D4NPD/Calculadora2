f2c6444cd8184ceb4f039c9a4173d242d4a419d3
Daniel Pizarro <danypd1012@gmail.com>
2022-10-06 03:08:38 -0500

    Calculadora terminada

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const display = document.getElementById('display'),
       keys = document.getElementById('keys')
 display.textContent = "0"
-let enOperacion = false
+let enOperacion = false,
+    resultado_mostrado = false,
+    numero1, tipoOperacion
 const calculadora = () =>{
     if(!keys) return 
     keys.addEventListener('click',e =>{
@@ -10,14 +12,17 @@ const calculadora = () =>{
         
         if(data.numero) imprimir(data.numero)
         if(data.operacion) operacion(target, data.operacion)
-        if(data.funcion) funcion()
+        if(data.funcion) funcion(data.funcion)
     })
 }
 
 function imprimir(numero){
+    resultado_mostrado = false
     if(display.textContent === '0' || enOperacion === true){
         display.textContent = numero
-    }else{
+    }else if(numero === '.' && !display.textContent.includes('.')){
+        display.textContent += numero
+    }else if(numero !== '.'){
         display.textContent += numero
     }
     enOperacion = false
@@ -25,9 +30,49 @@ function imprimir(numero){
 
 function operacion(elemento, operacion){
     enOperacion = true
-    const numero1 = display.textContent,
-          tipoOperacion = operacion
+    numero1 = Number(display.textContent),
+    tipoOperacion = operacion
     display.textContent = elemento.textContent
 }
+function resultado(){
+    if(resultado_mostrado){
+    }else{
+        let numero2 = Number(display.textContent),
+        result
+        switch(tipoOperacion){
+            case 'sumar':
+                result = numero1 + numero2
+                break
+            case 'restar':
+                result = numero1 - numero2
+                break
+            case 'multiplicar':
+                result = numero1 * numero2
+                break
+            case 'dividir': 
+                result = numero1 / numero2
+            default:
+                break
+    }
+        if(result === Infinity){
+            display.textContent = "Error: No se puede dividir entre cero"
+        }else{
+            display.textContent = result
+        }
+        resultado_mostrado = true
+    }
+    
+}
+function funcion(tipoFuncion){
+    if(tipoFuncion === 'clear-all'){
+        display.textContent = "0"
+    }else{
+        resultado()
+    }
+    enOperacion =  true
+}
+    
+
+
 
 calculadora()
